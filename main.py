2f156b33074944b53de922146f1607d4b406eb7d
Siddhesh Zantye <siddheshzantye@Siddheshs-MacBook-Pro.local>
2021-11-16 10:29:09 +1300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,64 @@
+from flask import Flask, redirect, request
+from flask.templating import render_template
+import json
+import pg
+
+HOMEURL = "192.168.1.68/8080"
+app = Flask("Url-Shortener")
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == "POST":
+        url = request.form['url']
+        for i in ['http://', 'https://']:
+            if i in url:
+                http = True
+            else:
+                http = False
+
+        if http == False:
+            return render_template("index.html", url=f"Url create failed! Url must contain http:// or https://")
+        with open('urls.json') as f:
+            data = json.load(f)
+        codes = []
+        for i in data:
+            codes.append(i["code"])
+        while True:
+            code = pg.gen_pass()
+            if code in codes:
+                pass
+            else:
+                break
+        newurl = {"code":code, "url":url}
+        data.append(newurl)
+        with open('urls.json', 'w') as f:
+            json.dump(data, f, indent=4)
+        return render_template("index.html", url=f"Url Created!     |     URL: --> {HOMEURL}/{code} <--")
+    else:
+        return render_template("index.html")
+
+
+@app.route('/<code>')
+def urlshort(code):
+    if len(code) == 5:
+        with open('urls.json') as f:
+            data = json.load(f)
+
+        for item in data:
+            if item['code'] == code:
+                rurl = item['url']
+            else:
+                rurl = False
+        
+        if rurl == False:
+            return "Invalid Code"
+        
+    else:
+        return "Invalid Code"
+
+    return redirect(rurl)
+
+
+# Run
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
